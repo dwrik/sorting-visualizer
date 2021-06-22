8e337795034ff2cf1e2a7c68d8bcdb5234de9668
Wrik Das <wrikdas22@gmail.com>
2021-06-22 23:08:23 +0530

    Fixed merge sort animation and refactored code

--- src/algorithms/MergeSort.js
+++ src/algorithms/MergeSort.js
@@ -1,146 +1,115 @@
-import {
-  BAR_STATE,
-  APP_STATE,
-  ANIMATION_SPEED,
-  ARRAY_FLASH_SPEED,
-} from "../App"
+import { BAR_STATE, APP_STATE, ANIMATION_SPEED, ARRAY_FLASH_SPEED } from "../App"
+import { ANIMATION_TYPE } from "../SortingController"
 
-const merge = (
-  originalArray,
-  auxiliaryArray,
-  start,
-  end,
-  mid,
-  animations,
-  sortedIdx
-) => {
+const merge = (originalArray, auxiliaryArray, start, mid, end, animations, sortedIndex) => {
   let k = start
   let i = start
   let j = mid + 1
   while (i <= mid && j <= end) {
-    animations.push([i, j, 1])
     if (auxiliaryArray[i].value < auxiliaryArray[j].value) {
-      animations.push([k, auxiliaryArray[i].value])
+      animations.push([k, j, ANIMATION_TYPE.comparison])
       originalArray[k++] = auxiliaryArray[i++]
     } else {
-      animations.push([k, auxiliaryArray[j].value])
+      animations.push([k, j, ANIMATION_TYPE.swap])
       originalArray[k++] = auxiliaryArray[j++]
+    }
+    if (start === 0 && end === originalArray.length - 1) {
+      sortedIndex.push(animations.length - 1)
     }
   }
   while (i <= mid) {
-    animations.push([k, auxiliaryArray[i].value])
     originalArray[k++] = auxiliaryArray[i++]
   }
   while (j <= end) {
-    animations.push([k, auxiliaryArray[j].value])
+    animations.push([k, j, ANIMATION_TYPE.swap])
     originalArray[k++] = auxiliaryArray[j++]
+    if (start === 0 && end === originalArray.length - 1) {
+      sortedIndex.push(animations.length - 1)
+    }
   }
 }
 
-const mergeSort = (
-  originalArray,
-  auxiliaryArray,
-  start,
-  end,
-  animations,
-  sortedIdx
-) => {
+const mergeSort = (originalArray, auxiliaryArray, start, end, animations, sortedIndex) => {
   if (start === end) return
   const mid = Math.floor((start + end) / 2)
-  mergeSort(auxiliaryArray, originalArray, start, mid, animations, sortedIdx)
-  mergeSort(auxiliaryArray, originalArray, mid + 1, end, animations, sortedIdx)
-  merge(originalArray, auxiliaryArray, start, end, mid, animations, sortedIdx)
+  mergeSort(auxiliaryArray, originalArray, start, mid, animations, sortedIndex)
+  mergeSort(auxiliaryArray, originalArray, mid + 1, end, animations, sortedIndex)
+  merge(originalArray, auxiliaryArray, start, end, mid, animations, sortedIndex)
 }
 
-const mergeSortHandler = (array, setArray, setState) => {
+const animateMergeSort = (array, setArray, setState) => {
   // change App state for disabling controls
   setState(APP_STATE.sorting)
 
-  let idx = 0 // sortedIdx pointer
-  const animations = []
-  const sortedIdx = []
+  // sortedIndex pointer
+  let idx = 0
 
-  mergeSort(
-    array.slice(),
-    array.slice(),
-    0,
-    array.length - 1,
-    animations,
-    sortedIdx
-  )
+  const animations = [] // stores comparison or swap animations
+  const sortedIndex = [] // stores animation indexes to highlight sorted bars
+
+  // get sorting animations
+  mergeSort(array.slice(), array.slice(), 0, array.length - 1, animations, sortedIndex)
 
   // process animations
-  // pair -> swap
-  // triplet -> comparison
-  animations.forEach((pair, index) => {
+  // animation -> [firstIndex, secondIndex, ANIMATION_TYPE]
+  animations.forEach((animation, index) => {
     const currentArray = array.slice()
-    if (pair[2] === 1) {
-      // change state
-      setTimeout(() => {
-        currentArray[pair[0]].state = BAR_STATE.compared
-        currentArray[pair[1]].state = BAR_STATE.compared
-        setArray(currentArray)
-      }, index * ANIMATION_SPEED)
 
-      // revert state
+    if (animation[2] === ANIMATION_TYPE.comparison) {
       setTimeout(() => {
-        currentArray[pair[0]].state = BAR_STATE.normal
-        currentArray[pair[1]].state = BAR_STATE.normal
+        currentArray[animation[0]].state = BAR_STATE.compared
+        currentArray[animation[1]].state = BAR_STATE.compared
         setArray(currentArray)
       }, index * ANIMATION_SPEED)
     } else {
-      // change state
       setTimeout(() => {
-        currentArray[pair[0]].state = BAR_STATE.swapped
+        currentArray[animation[0]].state = BAR_STATE.swapped
+        currentArray[animation[1]].state = BAR_STATE.swapped
         setArray(currentArray)
       }, index * ANIMATION_SPEED)
-
-      // change value
       setTimeout(() => {
-        currentArray[pair[0]].value = pair[1]
-        setArray(currentArray)
-      }, index * ANIMATION_SPEED)
-
-      // revert state
-      setTimeout(() => {
-        currentArray[pair[0]].state = BAR_STATE.normal
+        const swap = currentArray[animation[1]].value
+        for (let i = animation[1]; i > animation[0]; i--) {
+          currentArray[i].value = currentArray[i - 1].value
+        }
+        currentArray[animation[0]].value = swap
         setArray(currentArray)
       }, index * ANIMATION_SPEED)
     }
 
-    // if element at index is sorted
-    if (index === sortedIdx[idx]) {
-      idx++ // increment sortedIdx pointer
+    // revert state
+    setTimeout(() => {
+      currentArray[animation[0]].state = BAR_STATE.normal
+      currentArray[animation[1]].state = BAR_STATE.normal
+      setArray(currentArray)
+    }, (index + 1) * ANIMATION_SPEED)
 
-      // change state to sorted
+    // if element at index is sorted
+    if (index === sortedIndex[idx]) {
+      idx++
       setTimeout(() => {
-        currentArray[pair[0]].state = BAR_STATE.sorted
+        currentArray[animation[0]].state = BAR_STATE.sorted
         setArray(currentArray)
-      }, index * ANIMATION_SPEED)
+      }, (index + 1) * ANIMATION_SPEED)
     }
 
     // flash array to indicate sorting finished
     if (index === animations.length - 1) {
-      // change state to sorted
-      setTimeout(() => {
+      setTimeout(() => { // change state to sorted
         const sortedArray = array.map((bar) => {
           return { state: BAR_STATE.finished, value: bar.value }
         })
         setArray(sortedArray)
-
-        // change state to finished
-        setTimeout(() => {
+        setTimeout(() => { // change state to finished
           const finishedArray = array.map((bar) => {
             return { state: BAR_STATE.sorted, value: bar.value }
           })
           setArray(finishedArray)
-
-          // reset App state for enabling controls
-          setState(APP_STATE.default)
+          setState(APP_STATE.default) // reset App state for enabling controls
         }, ARRAY_FLASH_SPEED)
-      }, index * ANIMATION_SPEED)
+      }, (index + 1) * ANIMATION_SPEED)
     }
   })
 }
 
-export default mergeSortHandler
+export default animateMergeSort
